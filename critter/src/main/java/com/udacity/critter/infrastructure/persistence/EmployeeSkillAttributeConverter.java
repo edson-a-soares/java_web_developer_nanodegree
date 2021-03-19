e61de8f21ac190a01f9fe456d02afc559ac81a30
edson-a-soares <edson.araujo.soares@gmail.com>
2021-03-19 00:03:53 -0300

    Refactor for general improvements

--- critter/src/main/java/com/udacity/critter/infrastructure/persistence/EmployeeSkillAttributeConverter.java
+++ critter/src/main/java/com/udacity/critter/infrastructure/persistence/EmployeeSkillAttributeConverter.java
@@ -1,17 +1,15 @@
 package com.udacity.critter.infrastructure.persistence;
 
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.persistence.Converter;
 import javax.persistence.AttributeConverter;
 import com.udacity.critter.domain.model.user.EmployeeSkill;
 
 @Converter(autoApply = true)
-public class EmployeeSkillsAttributeConverter implements AttributeConverter<Set<EmployeeSkill>, String> {
+public class EmployeeSkillAttributeConverter implements AttributeConverter<Set<EmployeeSkill>, String> {
 
     @Override
     public String convertToDatabaseColumn(Set<EmployeeSkill> skills) {
